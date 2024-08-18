2890c6981d60adc5dabad11e54e42fdeed11f0c7
yannickdjoa <yannickdjoa2@gmail.com>
2024-08-18 13:36:53 +0200

    cookies screen finalized

---
+++ mobileApp-V1/components/generalComponents/TextButton.tsx
@@ -0,0 +1,40 @@
+import { View, Text, TouchableOpacity, ActivityIndicator } from "react-native";
+import React, { PropsWithChildren } from "react";
+
+type Props = PropsWithChildren<{
+  title: string;
+  containerStyles: string;
+  textStyle: string;
+  isLoading?: boolean;
+  handlePress: () => void;
+}>;
+const TextButton = ({
+  title,
+  containerStyles,
+  textStyle,
+  isLoading,
+  handlePress,
+}: Props) => {
+  return (
+    <TouchableOpacity
+      onPress={handlePress}
+      activeOpacity={0.7}
+      className={`bg-primary rounded-3xl min-h-[52px] px-4 flex flex-row justify-center items-center ${containerStyles} ${
+        isLoading ? "opacity-50" : ""
+      }`}
+      disabled={isLoading}
+    >
+      <Text className={`text-neutrals font-rbold text-lg ${textStyle}`}>{title}</Text>
+      {isLoading && (
+        <ActivityIndicator
+          animating={isLoading}
+          color="#fff"
+          size="small"
+          className="ml-2"
+        />
+      )}
+    </TouchableOpacity>
+  );
+};
+
+export default TextButton;
