e907711f3f0318905fcc8d68f39ad7936641adb3
yannickdjoa <yannickdjoa2@gmail.com>
2024-08-30 23:55:02 +0200

    update the text in userCreation

--- server/src/controllers/userProfile.controller.ts
+++ server/src/controllers/userProfile.controller.ts
@@ -14,7 +14,7 @@ const createUserProfile = async (req: Request, res: Response) => {
       return res.status(400).json({
         status: "failed",
         message:
-          "verifiez les cases obligatoires: firstName, surName, email, password, or phoneNumber.",
+          "verifiez les informations obligatoires: firstName, surName, email, password, or phoneNumber.",
       });
     }
 
