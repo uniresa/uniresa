f46b121ee391c7e0c24b3ef9450539a2b3ea774f
yannickdjoa <yannickdjoa2@gmail.com>
2024-08-30 11:44:27 +0200

    userProfile searchHistory and bookingDetails types set

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,25 @@
-
 import express, { Express, Request, Response } from "express";
-const port = 8000;
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
+const port = process.env.OUT_PORT || 8080;
+
+
+// exposedHeaders: ['set-cookie'];
+
+
+//  app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser());
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: 'https://www.yannickdjoa.org',
+//   })
+// );
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello, World! ts");
