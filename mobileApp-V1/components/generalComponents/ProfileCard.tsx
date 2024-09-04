a7fa80765c72922a19e30f91039ce961149eb5ec
yannickdjoa <yannickdjoa2@gmail.com>
2024-09-04 16:54:22 +0200

    logout functions implemented

--- mobileApp-V1/components/generalComponents/ProfileCard.tsx
+++ mobileApp-V1/components/generalComponents/ProfileCard.tsx
@@ -1,20 +1,22 @@
 import { View, Text, Image, Pressable } from "react-native";
 import React from "react";
-import { Profile } from "@/typesDeclaration/types";
+import { UserProfile } from "@/typesDeclaration/types";
 
 type ProfileCardProps = {
-  profile: Profile;
+  profile: UserProfile | null;
 };
 
 // ProfileCard component to display user's profile information in a card layout.
 const ProfileCard: React.FC<ProfileCardProps> = ({ profile }) => {
   // Extract the first letters of the first name and family name
-  const initials = `${profile.firstName.charAt(0)}${profile.surName.charAt(0)}`;
+  const initials = `${profile?.firstName.charAt(0)}${profile?.surName.charAt(
+    0
+  )}`;
   return (
     <View className="flex flex-row h-[150px] items-center justify-center bg-primary">
       <View className=" items-center justify-center gap-6">
         <View className=" rounded-full p-3 bg-secondary-100 border-2 border-secondary">
-          {profile.avatarUrl ? (
+          {profile?.avatarUrl ? (
             <Image
               source={{ uri: profile.avatarUrl }}
               className="w-12 h-12 rounded-full"
@@ -26,7 +28,7 @@ const ProfileCard: React.FC<ProfileCardProps> = ({ profile }) => {
           )}
         </View>
         <Text className="text-neutrals text-xl font-lbold">
-          {profile.firstName} {profile.surName}
+          {profile?.firstName} {profile?.surName}
         </Text>
       </View>
       <Pressable className="absolute right-4">
