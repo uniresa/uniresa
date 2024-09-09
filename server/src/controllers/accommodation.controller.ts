85027ad8cbb700e26b58734c93bebced10a71b36
yannickdjoa <yannickdjoa2@gmail.com>
2024-09-09 09:07:36 +0200

    get single and all accomodation server logics done

--- server/src/controllers/accommodation.controller.ts
+++ server/src/controllers/accommodation.controller.ts
@@ -2,6 +2,9 @@ import { Request, Response, NextFunction } from "express";
 import {
   AccommodationProperty,
   AvailabilityDetails,
+  BookingDetails,
+  Review,
+  RoomType,
 } from "../typesDeclaration/types";
 import { db } from "../../firebaseConfig";
 import { GenerateCustomID } from "../utils/customIdGenerator";
@@ -161,7 +164,7 @@ export const createAccommodation = async (req: Request, res: Response) => {
         const specificReviewId = reviewsRef.doc(GenerateCustomID(300));
         await specificReviewId.set({
           ...review,
-          reviewId: specificReviewId,
+          reviewId: specificReviewId.id,
           createdAt: new Date(),
           updatedAt: new Date(),
         });
@@ -191,7 +194,7 @@ export const createAccommodation = async (req: Request, res: Response) => {
     if (newAccommodation.roomTypes?.length) {
       for (const roomType of newAccommodation.roomTypes) {
         const specificRoomId = roomTypesRef.doc(GenerateCustomID(20));
-        await specificRoomId.set({ ...roomType, roomId: specificRoomId });
+        await specificRoomId.set({ ...roomType, roomId: specificRoomId.id });
 
         //availabilities subCollection
         const roomAvailabilitiesRef =
@@ -224,6 +227,155 @@ export const createAccommodation = async (req: Request, res: Response) => {
       status: "failed",
       message: "Error creating accommodation.",
       error,
+    });
+  }
+};
+export const getSingleAccommodation = async (req: Request, res: Response) => {
+  try {
+    const accommodationId = req.params.propertyId;
+    console.log(accommodationId);
+    const accommodationRef = db
+      .collection("accommodations")
+      .doc(accommodationId);
+    const accommodationData = await accommodationRef.get();
+
+    if (!accommodationData.exists) {
+      return res.status(404).json({
+        status: "failed",
+        message: "le logement n'existe pas dans notre base de données",
+      });
+    }
+    const accommodation = accommodationData.data() as AccommodationProperty;
+
+    // Fetch subcollection data concurrently
+    const [propertyBookingsSnapshot, reviewsSnapshot, roomTypesSnapshot] =
+      await Promise.all([
+        accommodationRef.collection("propertyBookings").get(),
+        accommodationRef.collection("reviews").get(),
+        accommodationRef.collection("roomTypes").get(),
+      ]);
+    // Map subcollection data to appropriate fields
+    accommodation.propertyBookings = propertyBookingsSnapshot.docs.map(
+      (doc) => doc.data() as BookingDetails
+    );
+
+    accommodation.reviews = reviewsSnapshot.docs.map(
+      (doc) => doc.data() as Review
+    );
+    accommodation.roomTypes = roomTypesSnapshot.docs.map(
+      (doc) => doc.data() as RoomType
+    );
+
+    // Fetch room availabilities concurrently
+    const roomAvailabilitiesPromises = accommodation.roomTypes.map(
+      async (roomType) => {
+        const roomAvailabilitiesSnapshot = await accommodationRef
+          .collection("roomTypes")
+          .doc(roomType.roomId)
+          .collection("roomAvailabilities")
+          .get();
+        return roomAvailabilitiesSnapshot.docs
+          .map((doc) => doc.data() as AvailabilityDetails)
+          .filter((availability) => availability.isAvailable); // Filter for available rooms
+      }
+    );
+    // Resolve all room availabilities promises
+    const allRoomAvailabilities = await Promise.all(roomAvailabilitiesPromises);
+    // Flatten the room availabilities array
+    accommodation.propertyAvailabilities = allRoomAvailabilities.flat();
+
+    return res.status(200).json({
+      status: "success",
+      message: "Logement récupéré avec succès.",
+      data: accommodation,
+    });
+  } catch (error) {
+    console.error("Error getting accommodation:", error);
+    return res.status(500).json({
+      status: "failed",
+      message: "Erreur lors de la récupération du logement",
+      error,
+    });
+  }
+};
+export const getAllAccommodations = async (req: Request, res: Response) => {
+  try {
+    const accommodationRef = db.collection("accommodations");
+    const accommodationsSnapshot = await accommodationRef.get();
+    const accommodations = accommodationsSnapshot.docs.map(
+      (doc) => doc.data() as AccommodationProperty
+    );
+
+    // Fetch all subcollection data concurrently for performance
+    const [propertyBookingsSnapshots, reviewsSnapshots, roomTypesSnapshots] =
+      await Promise.all([
+        Promise.all(
+          accommodations.map((acc) =>
+            accommodationRef
+              .doc(acc.propertyId)
+              .collection("propertyBookings")
+              .get()
+          )
+        ),
+        Promise.all(
+          accommodations.map((acc) =>
+            accommodationRef.doc(acc.propertyId).collection("reviews").get()
+          )
+        ),
+        Promise.all(
+          accommodations.map((acc) =>
+            accommodationRef.doc(acc.propertyId).collection("roomTypes").get()
+          )
+        ),
+      ]);
+
+    // Map subcollection data to appropriate fields
+    accommodations.forEach((accommodation, index) => {
+      // Set property bookings
+      accommodation.propertyBookings = propertyBookingsSnapshots[
+        index
+      ].docs.map((doc) => doc.data() as BookingDetails);
+
+      // Set reviews
+      accommodation.reviews = reviewsSnapshots[index].docs.map(
+        (doc) => doc.data() as Review
+      );
+
+      // Set room types
+      accommodation.roomTypes = roomTypesSnapshots[index].docs.map(
+        (doc) => doc.data() as RoomType
+      );
+    });
+
+    // Fetch room availabilities concurrently for all room types
+    const roomAvailabilitiesPromises = accommodations.flatMap((accommodation) =>
+      accommodation.roomTypes.map(async (roomType) => {
+        const roomAvailabilitiesSnapshot = await accommodationRef
+          .doc(accommodation.propertyId)
+          .collection("roomTypes")
+          .doc(roomType.roomId)
+          .collection("roomAvailabilities")
+          .get();
+        roomType.roomAvailabilities = roomAvailabilitiesSnapshot.docs
+          .map((doc) => doc.data() as AvailabilityDetails)
+          .filter((availability) => availability.isAvailable); // Filter for available rooms
+      })
+    );
+
+    // Wait for all room availabilities to be fetched
+    await Promise.all(roomAvailabilitiesPromises);
+
+    return res.status(200).json({
+      status: "success",
+      message: "Tous les logements récupérés avec succès.",
+      data: accommodations,
+    });
+  } catch (error: any) {
+    console.error("Error getting all accommodations:", error);
+    return res.status(500).json({
+      status: "failed",
+      message: "Erreur lors de la récupération des logements.",
+      error: error.message,
     });
   }
 };
