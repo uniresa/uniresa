a98d3c9662c53ac25e74013cf9f63b5d6c42362c
yannickdjoa <yannickdjoa2@gmail.com>
2024-09-26 21:19:41 +0200

    photos rendering in accomotion screen done

--- mobileApp-V1/app/(search)/[propertyId].tsx
+++ mobileApp-V1/app/(search)/[propertyId].tsx
@@ -1,19 +1,64 @@
-import { View, Text, Image, Animated } from "react-native";
-import React, { useState } from "react";
+import { View, Text, Image, Animated, ImageBackground } from "react-native";
+import React, { useRef, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { router, useLocalSearchParams } from "expo-router";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
 import CustomButton from "@/components/generalComponents/CustomButton";
+import renderStars from "@/utils/renderStars";
+import { AccommodationProperty } from "@/typesDeclaration/types";
+import { Dimensions } from "react-native";
 
+const { width } = Dimensions.get("window");
 const accommodationOverviewPage = () => {
-  const { propertyName } = useLocalSearchParams();
+  const params = useLocalSearchParams();
+  const [isExpanded, setIsExpanded] = useState(false);
+  const toggleExpanded = () => {
+    setIsExpanded((prevState) => !prevState); // Toggle between expanded and collapsed
+  };
+  let parsedProperty;
+  try {
+    if (params.property) {
+      const propertyString = Array.isArray(params.property)
+        ? params.property[0]
+        : params.property;
+
+      parsedProperty = JSON.parse(propertyString) as AccommodationProperty;
+    } else {
+      console.error("params.property is undefined or null");
+      parsedProperty = {
+        propertyName: "",
+        description: "",
+        numberOfStars: 0,
+        images: [],
+      };
+    }
+  } catch (error) {
+    console.error("Failed to parse property:", error);
+    parsedProperty = {
+      propertyName: "",
+      description: "",
+      numberOfStars: 0,
+      images: [],
+    }; // Default value
+  }
+  const { propertyName, description, numberOfStars, images } = parsedProperty;
   const [scrollY, setScrollY] = useState(new Animated.Value(0));
+  const [currentIndex, setCurrentIndex] = useState(0); // Track the current image index
+  const scrollX = useRef(new Animated.Value(0)).current; // For horizontal scrolling
+
   // Interpolation to show/hide property name based on scroll position
   const propertyNameOpacity = scrollY.interpolate({
     inputRange: [0, 70], // adjust the second value to control when the property name fades
     outputRange: [0, 1], // not visible at top, appears as you scroll
     extrapolate: "clamp",
   });
+  const handleScroll = (event: {
+    nativeEvent: { contentOffset: { x: any } };
+  }) => {
+    const contentOffsetX = event.nativeEvent.contentOffset.x;
+    const index = Math.round(contentOffsetX / width); // Update the current index based on scroll position
+    setCurrentIndex(index);
+  };
   return (
     <SafeAreaView className="flex-1 bg-neutrals-20">
       {/* Fixed Header */}
@@ -65,12 +110,75 @@ const accommodationOverviewPage = () => {
         )}
         scrollEventThrottle={16}
       >
-        <View className="mt-4 px-4">
+        <View className="mt-4 px-2">
           <Text className="font-lbold text-3xl text-neutrals-900">
             {propertyName}
           </Text>
+          <View className="flex flex-row items-center gap-1 mt-1">
+            {renderStars(numberOfStars, 24)}
+          </View>
+          {/* FlatList to render images */}
+          <View className="flex flex-row w-full justify-between gap-1 mt-2">
+            <FlatList
+              data={images}
+              keyExtractor={(item, index) => index.toString()}
+              onScroll={handleScroll}
+              pagingEnabled={true}
+              showsHorizontalScrollIndicator={false}
+              horizontal={true}
+              // contentContainerStyle={{ paddingHorizontal: 10 }}
+              renderItem={({ item }) => (
+                <TouchableOpacity onPress={() => {}}>
+                  <ImageBackground
+                    source={{ uri: item, cache: "force-cache" }}
+                    className="h-[300px] w-full mx-2"
+                    style={{ width: width }}
+                    resizeMode="cover"
+                  />
+                </TouchableOpacity>
+              )}
+            />
+            <View className="absolute right-0 bottom-0 bg-neutrals-800 p-2">
+              <Text className="text-base text-neutrals">
+                {images.length} photos
+              </Text>
+            </View>
+          </View>
+          {/* Custom Image Indicator */}
+          <View className="flex flex-row justify-center mt-4">
+            {images.map((_, index) => (
+              <View
+                key={index}
+                style={{
+                  height: 10,
+                  width: 10,
+                  borderRadius: 5,
+                  marginHorizontal: 5,
+                  backgroundColor: currentIndex === index ? "#ffa500" : "gray",
+                }}
+              />
+            ))}
+          </View>
+          <Text className="font-lbold text-xl text-neutrals-900"></Text>
           <Text style={{ marginVertical: 300 }}>Scrollable content here</Text>
-          <Text style={{ marginVertical: 300 }}>More content below</Text>
+          <View className="mt-4 mx-4">
+            <Text className="text-neutrals-900 font-lbold text-3xl mb-4 flex-wrap">
+              Description
+            </Text>
+            <View>
+              <Text
+                className="text-neutrals-800 font-semibold text-xl text-justify"
+                numberOfLines={isExpanded ? undefined : 4}
+              >
+                {description}
+              </Text>
+              <TouchableOpacity onPress={toggleExpanded}>
+                <Text className="text-warning-600 text-xl my-4">
+                  {isExpanded ? "Voir moins" : "Voir la description complete"}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
       </Animated.ScrollView>
       {/* Fixed Footer */}
